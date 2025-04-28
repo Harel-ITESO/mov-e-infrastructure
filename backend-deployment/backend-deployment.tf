04fe059123d1b05873a0b4f36d6e212a247ec281
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-04-28 12:00:14 -0600

    tf code works

--- backend-deployment/backend-deployment.tf
+++ backend-deployment/backend-deployment.tf
@@ -2,103 +2,124 @@ variable "REGION" {}
 variable "VPC" {}
 variable "SUBNETS" {}
 variable "ROLE" {}
-variable "DOCKER_IMAGE" {}
-variable "APP_INSTANCES" {}
+variable "EC2_AMI" {}
+variable "EC2_TYPE" {}
+variable "EC2_KEY" {}
+variable "EC2_INSTANCES" {}
+variable "ECR_REPOSITORY" {}
+variable "ECR_IMAGE" {}
+variable "APP_PORT" {}
+variable "CONTAINER_PORT" {}
+variable "APP_HEALTH" {}
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+variable "AWS_SESSION_TOKEN" {}
 
 provider "aws" {
-  region = var.REGION
+  region     = var.REGION
 }
 
 resource "aws_security_group" "alb_sg" {
-  name        = "alb-security-group"
-  vpc_id      = var.VPC
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "alb_1" {
-  name               = "alb-1"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups = [aws_security_group.alb_sg.id]
-  subnets            = var.SUBNETS
-  enable_deletion_protection = false
-}
-
-resource "aws_lb_target_group" "alb_1_target_group" {
-  name     = "alb-1-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = var.VPC
-  target_type = "ip"
-}
-
-resource "aws_lb_listener" "alb_1_listener" {
-  load_balancer_arn = aws_lb.alb_1.arn
-  port              = 80
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_1_target_group.arn
-  }
-}
-
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "ecs-cluster"
-}
-
-resource "aws_ecs_task_definition" "app_1" {
-  family                   = "app-1"
-  execution_role_arn       = var.ROLE
-  task_role_arn            = var.ROLE
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "1024"
-  memory                   = "4096"
-  container_definitions = jsonencode([
-    {
-      name      = "app-1"
-      image     = var.DOCKER_IMAGE
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        }
-      ]
+    ingress {
+        from_port   = var.APP_PORT
+        to_port     = var.APP_PORT
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
-  ])
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
-resource "aws_ecs_service" "service_1" {
-  name            = "service-1"
-  cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.app_1.arn
-  desired_count   = var.APP_INSTANCES
-  launch_type     = "FARGATE"
-  network_configuration {
-    subnets          = var.SUBNETS
-    security_groups  = [aws_security_group.alb_sg.id]
-    assign_public_ip = true
-  }
-  load_balancer {
-    target_group_arn = aws_lb_target_group.alb_1_target_group.arn
-    container_name   = "app-1"
-    container_port   = 80
-  }
+resource "aws_security_group" "ec2_sg" {
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port   = var.APP_PORT
+        to_port     = var.APP_PORT
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
-output "alb_1_dns" {
-  value = aws_lb.alb_1.dns_name
+resource "aws_instance" "ec2_instances" {
+    ami             = var.EC2_AMI
+    instance_type   = var.EC2_TYPE
+    count           = var.EC2_INSTANCES
+    key_name        = var.EC2_KEY
+    subnet_id       = var.SUBNETS[0]
+    security_groups = [aws_security_group.ec2_sg.id]
+    user_data = <<-EOF
+      #!/bin/bash
+      sudo su
+      exec > /var/log/user-data.log 2>&1
+      yum update -y
+      yum install -y docker
+      service docker start
+      export AWS_ACCESS_KEY_ID="${var.AWS_ACCESS_KEY_ID}"
+      export AWS_SECRET_ACCESS_KEY="${var.AWS_SECRET_ACCESS_KEY}"
+      export AWS_SESSION_TOKEN="${var.AWS_SESSION_TOKEN}"
+      export AWS_DEFAULT_REGION="${var.REGION}"
+      aws ecr get-login-password --region ${var.REGION} | docker login --username AWS --password-stdin ${var.ECR_REPOSITORY}
+      docker pull ${var.ECR_IMAGE}
+      docker run -d --restart always -p ${var.APP_PORT}:${var.CONTAINER_PORT} ${var.ECR_IMAGE}
+    EOF
+}
+
+resource "aws_lb" "alb" {
+    name            = "alb"
+    internal        = false
+    ip_address_type     = "ipv4"
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.alb_sg.id]
+    subnets = var.SUBNETS
+}
+
+resource "aws_lb_target_group" "target_group" {
+    health_check {
+        interval            = 10
+        path                = var.APP_HEALTH
+        protocol            = "HTTP"
+        timeout             = 5
+        healthy_threshold   = 5
+        unhealthy_threshold = 2
+    }
+    name          = "target-group"
+    port          = var.APP_PORT
+    protocol      = "HTTP"
+    target_type   = "instance"
+    vpc_id = var.VPC
+}
+
+resource "aws_lb_listener" "listener" {
+    load_balancer_arn          = aws_lb.alb.arn
+    port                       = var.APP_PORT
+    protocol                   = "HTTP"
+    default_action {
+        target_group_arn         = aws_lb_target_group.target_group.arn
+        type                     = "forward"
+    }
+}
+
+resource "aws_lb_target_group_attachment" "ec2_attach" {
+    count = length(aws_instance.ec2_instances)
+    target_group_arn = aws_lb_target_group.target_group.arn
+    target_id        = aws_instance.ec2_instances[count.index].id
+}
+
+output "alb_dns" {
+  value = aws_lb.alb.dns_name
 }
